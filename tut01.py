ab7b27ed1ff34561e4b1edc1bfc5b5c719770dae
Natasha <znatashka@ya.ru>
2016-11-14 09:39:04 +0300

    tut01 - tut05

---
+++ tut01.py
@@ -0,0 +1,11 @@
+import cherrypy
+
+
+class HelloWorld:
+    @cherrypy.expose
+    def index(self):
+        return 'Hello World!'
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(HelloWorld())
---
+++ tut03.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+import cherrypy
+
+
+class StringGenerator:
+    @cherrypy.expose
+    def index(self):
+        return 'Hello World!'
+
+    @cherrypy.expose
+    def generate(self, length=8):
+        return ''.join(random.sample(string.hexdigits, int(length)))
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(StringGenerator())
